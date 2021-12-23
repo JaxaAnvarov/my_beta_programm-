2002f9e14856be72be25e83522d0f7ede0453d69
JaxaAnvarov <anvarovjahongir13@gmail.com>
2021-12-23 18:26:20 +0500

    added new dependencies and sort folders

---
+++ lib/core/constants/export.dart
@@ -0,0 +1,3 @@
+export 'package:my_beta_programm/core/constants/size_config.dart';
+export 'package:my_beta_programm/screens/home/home_page/home_page.dart';
+export 'package:my_beta_programm/core/constants/colors.dart';
