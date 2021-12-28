cb920a4806a43556acb8d175cc78b7048ae1b024
JaxaAnvarov <anvarovjahongir13@gmail.com>
2021-12-28 17:05:02 +0500

    ticked sahifasi yengilandi

---
+++ lib/core/model/mock/list_for_width.dart
@@ -0,0 +1,19 @@
+class Defaults{
+  static const List<double> width = [
+    106.0,
+    132.0,
+    115.0,
+    106.0,
+    132.0,
+    115.0,
+  ];
+
+  static const List<String> text = [
+    'Phil Collins',
+    'TV in the Radio',
+    'FC Barcelona',
+    'Phil Collins',
+    'TV in the Radio',
+    'FC Barcelona',
+  ];
+}
--- lib/screens/ticket/ticket_page.dart
+++ lib/screens/ticket/ticket_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_beta_programm/core/constants/export.dart';
+import 'package:my_beta_programm/core/model/mock/list_for_width.dart';
 
 class TickedPage extends StatelessWidget {
   const TickedPage({Key? key}) : super(key: key);
@@ -15,15 +16,70 @@ class TickedPage extends StatelessWidget {
               height: getProportionateScreenHeight(552.0),
               width: double.infinity,
               decoration: CDecoration().boxDecoration(
-                  radius: getProportionateScreenWidth(0.0),
-                  imageUrl: Images.bgFon,
-                  color: ProjectColors().kPrimaryWhiteColor),
+                radius: getProportionateScreenWidth(0.0),
+                imageUrl: Images.bgFon,
+                color: ProjectColors().kPrimaryWhiteColor,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: getProportionateScreenHeight(102),
+                      right: getProportionateScreenWidth(251.0),
+                      left: getProportionateScreenWidth(16.0),
+                    ),
+                    width: getProportionateScreenWidth(120.0),
+                    child: ConstantsText(
+                      color: ProjectColors().kPrimaryWhiteColor,
+                      size: getProportionateScreenWidth(28.0),
+                      text: 'Popular',
+                    ),
+                  ),
+                  SizedBox(
+                    height: getProportionateScreenHeight(40.0),
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 6,
+                      itemBuilder: (
+                        BuildContext _,
+                        int __,
+                      ) {
+                        return Padding(
+                          padding: DefaultPaddigs.symmetric(
+                            horizontal: getProportionateScreenHeight(16.0),
+                          ),
+                          child: Container(
+                            alignment: Alignment.center,
+                            height: getProportionateScreenHeight(32.0),
+                            width: getProportionateScreenWidth(
+                              Defaults.width[__].toDouble(),
+                            ),
+                            decoration: CDecoration().boxDecoration(
+                              color: ProjectColors()
+                                  .kPrimaryWhiteColor
+                                  .withOpacity(0.3),
+                              radius: getProportionateScreenWidth(20.0),
+                              imageUrl: '',
+                            ),
+                            child: ConstantsText(
+                              text: Defaults.text[__].toString(),
+                              color: ProjectColors().kPrimaryWhiteColor,
+                              size: getProportionateScreenWidth(14.0),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
             ConstantsText(
               color: ProjectColors().kPrimaryTextColor,
               text: 'No tickets bought',
               size: getProportionateScreenWidth(28.0),
             ),
+            SizedBox(height: getProportionateScreenHeight(17.0)),
             ConstantsText(
               color: ProjectColors().kPrimatySecondaryTextColor,
               size: getProportionateScreenWidth(15.0),
